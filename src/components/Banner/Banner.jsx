d1d63b2554f78a3ae8d464a8dbeb06e9c1eb70dd
MannySanz <mannysanz715@gmail.com>
2023-05-07 15:38:37 -0700

    Styling Stuff

--- src/components/Banner/Banner.jsx
+++ src/components/Banner/Banner.jsx
@@ -4,7 +4,7 @@ function Banner(){
     <header>
       <h2 className="banner-title">Driving Instructors in Los Angeles</h2>
       <h3 className="banner-description">Driving Lessons for teenagers</h3>
-      <h3 className="banner-action">Call to schedule your appointment</h3>
+      <a className='phone-tag' href="tel:+1(213)841-1290"><h3 className="banner-action">Call to schedule your appointment</h3></a>
     </header>
 )
 }
