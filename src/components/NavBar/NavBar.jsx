7e767cb128c4cc3f5ab0294d30d6b739fcbbd22e
MannySanz <mannysanz715@gmail.com>
2023-05-15 15:18:49 -0700

    Link Nav Logo to Home Page

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -1,9 +1,9 @@
 import './NavBar.css'
-
+import { Link } from 'react-router-dom'
 function NavBar(){
   return (
     <nav>
-      <img className='logo' src="/assets/logo.png" alt="school-logo"/>
+      <Link to='/'><img className='logo' src="/assets/logo.png" alt="school-logo"/></Link>
       <img className='menu-buttons' src="/assets/menuButtons.png" alt="menu"/>
     </nav>
 )
