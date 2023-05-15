bd1cd58571aa3da0455e5738c066653f4b495870
MannySanz <mannysanz715@gmail.com>
2023-05-15 15:25:48 -0700

    Typo Fix 30$ to

--- src/components/Policies/Policies.jsx
+++ src/components/Policies/Policies.jsx
@@ -6,7 +6,7 @@ const Policies = () => {
       <ul className="policies-list">
         <li className="policy-item">All Lessons Must Be Pre-Paid In Order To Keep Your Scheduled Appointment</li>
         <li>All Paid Lessons Must Be Completed Within 3 Months From The Date Of Purchase</li>
-        <li>Cancellations Within 24 Hours Of The Class Will Be Subject To A 30$ Fee</li>
+        <li>Cancellations Within 24 Hours Of The Class Will Be Subject To A $30 Fee</li>
         <li>All Driving Appointments Are 2 Hours</li>
         <li>All Duplicates Are Subject To A $25.00 Fee</li>
       </ul>
