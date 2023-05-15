5b4f8bd4c3f362b05f59cc29e473c8e94a0c5cba
MannySanz <mannysanz715@gmail.com>
2023-05-15 15:16:22 -0700

    Start Policies Styling. Heading and Margin

--- src/components/Policies/Policies.jsx
+++ src/components/Policies/Policies.jsx
@@ -1,15 +1,16 @@
+import './Policies.css'
 const Policies = () => {
   return (
-    <>
-      <h3>Policies List</h3>
-      <ul>
-        <li>All Lessons Must Be Pre-Paid In Order To Keep Your Scheduled Appointment</li>
+    <div className='policies-container'>
+      <h1 className='policies-heading'>Policies List</h1>
+      <ul className="policies-list">
+        <li className="policy-item">All Lessons Must Be Pre-Paid In Order To Keep Your Scheduled Appointment</li>
         <li>All Paid Lessons Must Be Completed Within 3 Months From The Date Of Purchase</li>
         <li>Cancellations Within 24 Hours Of The Class Will Be Subject To A 30$ Fee</li>
         <li>All Driving Appointments Are 2 Hours</li>
         <li>All Duplicates Are Subject To A $25.00 Fee</li>
       </ul>
-    </>
+    </div>
 )
 }
 
