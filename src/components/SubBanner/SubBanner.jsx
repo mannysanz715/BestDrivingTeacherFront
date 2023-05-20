0db8c26494fe011a259a77c2d1158c629eeeea70
mannysanz715 <mannysanz715@gmail.com>
2023-05-19 21:08:06 -0700

    Import Animation on Scroll for react

--- src/components/SubBanner/SubBanner.jsx
+++ src/components/SubBanner/SubBanner.jsx
@@ -1,11 +1,19 @@
 import './SubBanner.css'
+import Aos from 'aos'
+import 'aos/dist/aos.css'
+import { useEffect } from 'react'
 function SubBanner(){
+  useEffect(()=>{
+    Aos.init({})
+  },[])
   return (
-    <div id='about-us' className='sub-banner-container'>
-      <h2 className='sub-banner-title'>About Graces Driving School</h2>
+    <div className='sub-banner-container'>
+      <h2 className='sub-banner-title'>About<br /> Graces Driving School</h2>
       <hr className='line'/>
-      <p className='sub-banner-text'>A Driving School that has taught teens and nervous drivers for over 15 years to overcome the challenges of driving on our traffic congested streets and highways. Teaching teens how to drive does not have to be stressful or dangerous. If you are a nervous driver and can not bear to get in the car with your teen behind the wheel, then we are here to help you.</p>
-      <img src="/assets/grace.png" className='grace-img'  />
+      <div id='about-us' className='sub-banner-info'>
+        <p className='sub-banner-text'>A Driving School that has taught teens and nervous drivers for over 15 years to overcome the challenges of driving on our traffic congested streets and highways. Teaching teens how to drive does not have to be stressful or dangerous. If you are a nervous driver and can not bear to get in the car with your teen behind the wheel, then we are here to help you.</p>
+        <img src="/assets/grace.png" className='grace-img'  />
+      </div>
     </div>
 )
 }
