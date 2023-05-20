6e1b6ef54494c7213160bb912ec8e79d12d1f331
mannysanz715 <mannysanz715@gmail.com>
2023-05-19 21:50:45 -0700

    Re-add animation

--- src/components/SubBanner/SubBanner.jsx
+++ src/components/SubBanner/SubBanner.jsx
@@ -12,7 +12,7 @@ function SubBanner(){
         <hr className='line'/>
       <div id='about-us' className='sub-banner-info'>
         <p className='sub-banner-text'>A Driving School that has taught teens and nervous drivers for over 15 years to overcome the challenges of driving on our traffic congested streets and highways. Teaching teens how to drive does not have to be stressful or dangerous. If you are a nervous driver and can not bear to get in the car with your teen behind the wheel, then we are here to help you.</p>
-        <img src="/assets/grace.png" className='grace-img'  />
+        <img data-aos='fade-right' src="/assets/grace.png" className='grace-img'  />
       </div>
     </div>
 )
