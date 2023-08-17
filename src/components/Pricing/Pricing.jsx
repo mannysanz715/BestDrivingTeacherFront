707644f7ecea5b87b08194153c08a23753ee2457
MannySanz <mannysanz715@gmail.com>
2023-08-17 08:42:12 -0700

    Create new card with lesson packages

--- src/components/Pricing/Pricing.jsx
+++ src/components/Pricing/Pricing.jsx
@@ -18,20 +18,30 @@ useEffect(()=>{
 
         <div data-aos='fade-up' className="reg-class-pricing card">
           <h3 className='rate-title'>2 Hour Lessons</h3>
-          <p className='rate-price'>Price: $150</p>
+          <p className='rate-price'> Price: $175</p>
+          <h4 className='center-text'>Here is what is included in the lessons</h4>
           <p className='rate-description'>Pick up and drop off within the Los Angeles area.</p>
           <p className='rate-description'>Training during the entire lesson.</p>
           <p className='rate-description'>Private lesson with patient and experienced instructors.</p>
           <p className='rate-description'>Confidence with safetey features such as dual controll systems for instructors.</p>
         </div>
+        <div data-aos='fade-up' className="drive-test-pricing card">
+          <h3 className='rate-title'>Lesson Packages</h3>
+          <p className='rate-price'> 3 Lessons : $450</p>
+          <p className='rate-description center-text'>DMV 6 Hour Certificate Compliant.</p>
+          <p className='rate-description center-text'>Inlcudes everything the single lesson includes.</p>
+          <p className='rate-price'> 5 Lessons : $700</p>
+          <p className='rate-description center-text'>DMV 6 Hour Certificate Compliant.</p>
+          <p className='rate-description center-text'>Inlcudes everything the single lesson includes.</p>
+        </div>
         <div data-aos='fade-up' className="drive-test-pricing card end">
           <h3 className='rate-title'>Behind the Wheel Test</h3>
-          <p className='rate-price'>Price: $225</p>
+          <p className='rate-price'>Price: $260</p>
           <p className='rate-description'>Pick up within the Los Angeles area 1 hour prior to DMV appointment time.</p>
           <p className='rate-description'>All registration and paper work taken care of with DMV.</p>
           <p className='rate-description'>Vehicle for the driving exam.</p>
           <p className='rate-description'>And drop off in the Los Angeles area.</p>
-          <p className='rate-description'><strong>This is a flat rate, whether student passes or fails</strong></p>
+          <p className='rate-description center-text'><strong>This is a flat rate, whether student passes or fails</strong></p>
         </div>
         </div>
     </>
