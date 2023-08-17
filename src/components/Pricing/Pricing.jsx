bc83a6020c0daa3a89caeafd6ffa409485783cd2
MannySanz <mannysanz715@gmail.com>
2023-08-17 08:53:11 -0700

    Create package names and change test price to 250

--- src/components/Pricing/Pricing.jsx
+++ src/components/Pricing/Pricing.jsx
@@ -27,16 +27,16 @@ useEffect(()=>{
         </div>
         <div data-aos='fade-up' className="drive-test-pricing card">
           <h3 className='rate-title'>Lesson Packages</h3>
-          <p className='rate-price'> 3 Lessons : $450</p>
+          <p className='rate-price'> Silver Package : $450</p>
+          <p className='rate-description center-text'>Inlcudes  3 Driving Lessons, Each 2 Hours.</p>
           <p className='rate-description center-text'>DMV 6 Hour Certificate Compliant.</p>
-          <p className='rate-description center-text'>Inlcudes everything the single lesson includes.</p>
-          <p className='rate-price'> 5 Lessons : $700</p>
+          <p className='rate-price'> Gold Package : $700</p>
+          <p className='rate-description center-text'>Inlcudes  5 Driving Lessons, Each 2 Hours.</p>
           <p className='rate-description center-text'>DMV 6 Hour Certificate Compliant.</p>
-          <p className='rate-description center-text'>Inlcudes everything the single lesson includes.</p>
         </div>
         <div data-aos='fade-up' className="drive-test-pricing card end">
           <h3 className='rate-title'>Behind the Wheel Test</h3>
-          <p className='rate-price'>Price: $260</p>
+          <p className='rate-price'>Price: $250</p>
           <p className='rate-description'>Pick up within the Los Angeles area 1 hour prior to DMV appointment time.</p>
           <p className='rate-description'>All registration and paper work taken care of with DMV.</p>
           <p className='rate-description'>Vehicle for the driving exam.</p>
