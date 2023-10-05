e89c2d3c21bdc9a9e020688ff57080dcf6d39c4f
Manny Sanchez <mannysanz715@gmail.com>
2023-10-05 11:14:39 -0700

    Fix map and add embedded map

--- src/components/ActionBanner/ActionBanner.jsx
+++ src/components/ActionBanner/ActionBanner.jsx
@@ -1,3 +1,4 @@
+/* eslint-disable react/no-unknown-property */
 import './ActionBanner.css'
 
 function ActionBanner() {
@@ -8,7 +9,10 @@ function ActionBanner() {
         +1 (213) 814 - 1290
       </a>
       <h1 className="action-banner-title">Location:</h1>
-      <p className='action-banner-button'>925 N Labrea, 4th Floor, Suite 429</p>
+      <a href='https://maps.app.goo.gl/LYTBsovExZPexkPL8' target='_blank' className='address-link' rel="noreferrer">
+        <p className='action-banner-button'>925 N La Brea 4th Floor, Suite 429</p>
+      </a>
+      <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3304.281300015654!2d-118.34721742355485!3d34.087933515843716!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x29116d75c752fe7d%3A0x9bd4234f1d64c98d!2sGrace&#39;s%20Driving%20School!5e0!3m2!1sen!2sus!4v1696529335583!5m2!1sen!2sus" width="350" height="350"  loading="lazy"></iframe>
 
     </div>
 
