7ccbba3ffcd9f0d7acdb7ad5eb2fe78b6a56e5e6
Manny Sanchez <mannysanz715@gmail.com>
2023-10-06 09:40:06 -0700

    Update Test Description

--- src/components/Pricing/Pricing.jsx
+++ src/components/Pricing/Pricing.jsx
@@ -13,8 +13,8 @@ useEffect(()=>{
   return (
     <>
       <h2 className='pricing-header' id='pricing'>Driving Rates</h2>
-        <Link onClick={scrollToTop} className='policies-link' to='/policies#policies-container'>BEFORE BOOKING, CLICK HERE TO GO OVER OUR POLICIES</Link>
-        <div className='pricing-card-container'>
+      <Link onClick={scrollToTop} className='policies-link' to='/policies#policies-container'>BEFORE BOOKING, CLICK HERE TO GO OVER OUR POLICIES</Link>
+      <div className='pricing-card-container'>
 
         <div data-aos='fade-up' className="reg-class-pricing card">
           <h3 className='rate-title'>2 Hour Lessons</h3>
@@ -25,15 +25,17 @@ useEffect(()=>{
           <p className='rate-description'>Private lesson with patient and experienced instructors.</p>
           <p className='rate-description'>Confidence with safety features such as dual controll systems for instructors.</p>
         </div>
+        
         <div data-aos='fade-up' className="drive-test-pricing card">
           <h3 className='rate-title'>Lesson Packages</h3>
           <p className='rate-price'> Silver Package : $450</p>
-          <p className='rate-description center-text'>Inlcudes  3 lessons(6 hours).</p>
           <p className='rate-description center-text'>DMV 6 Hour Certificate Compliant.</p>
+          <p className='rate-description center-text'>Inlcudes  3 lessons(6 hours).</p>
           <p className='rate-price'> Gold Package : $700</p>
           <p className='rate-description center-text'>Inlcudes  5 lessons(10 hours).</p>
           <p className='rate-description center-text'>DMV 6 Hour Certificate Compliant.</p>
         </div>
+        
         <div data-aos='fade-up' className="drive-test-pricing card end">
           <h3 className='rate-title'>Behind the Wheel Test</h3>
           <p className='rate-price'>Price: $250</p>
@@ -41,9 +43,8 @@ useEffect(()=>{
           <p className='rate-description'>All registration and paper work taken care of with DMV.</p>
           <p className='rate-description'>Vehicle for the driving exam.</p>
           <p className='rate-description'>And drop off in the Los Angeles area.</p>
-          <p className='rate-description center-text'><strong>This is a flat rate, whether student passes or fails</strong></p>
         </div>
-        </div>
+      </div>
     </>
 )
 }
